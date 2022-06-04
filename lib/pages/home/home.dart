5c48df7a669d7454dec7819ee0659d3a4717ea01
otoraja-ahmad <ahmad.wahyudi@otoraja.co.id>
2022-06-04 09:03:04 +0700

    set layoutbuilder for web

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,4 +1,3 @@
-import 'dart:html';
 import 'dart:io';
 
 import 'package:carousel_slider/carousel_slider.dart';
@@ -51,158 +50,159 @@ class _HomeScreenState extends State<HomeScreen> {
 
     final List<Widget> _widgetOptions = <Widget>[
       SingleChildScrollView(
-        child: Container(
-          width: kIsWeb ? MediaQuery.of(context).size.width * 1 : MediaQuery.of(context).size.width,
-          child: Container(
-
-            margin: kIsWeb ? EdgeInsets.symmetric(horizontal: 300) : EdgeInsets.all(0),
-            child: Column(
-              children: [
-                Container(
-                    color: Colors.white,
-                    padding: EdgeInsets.all(10),
-                    margin: EdgeInsets.fromLTRB(10, 0, 10, 5),
-                    child: SectionOne()),
-                Container(
-                    color: Colors.white,
-                    padding: EdgeInsets.only(left: 10, right: 10),
-                    margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
-                    child: Stack(
-                      children: [
-                        CarouselSlider(
-                            items: bannersWidget,
-                            options: CarouselOptions(
-                              aspectRatio: 16 / 9,
-                              viewportFraction: 1.0,
-                              initialPage: 0,
-                              enableInfiniteScroll: true,
-                              reverse: false,
-                              autoPlay: true,
-                              onPageChanged: (index, reason) {
-                                setState(() {
-                                  _carouselIndex = index;
-                                });
-                              },
-                              autoPlayInterval: Duration(seconds: 3),
-                              autoPlayAnimationDuration:
-                                  Duration(milliseconds: 800),
-                              autoPlayCurve: Curves.fastOutSlowIn,
-                              enlargeCenterPage: true,
-                              scrollDirection: Axis.horizontal,
-                            )),
-                        Positioned(
-                          bottom: 0,
-                          top: 200,
-                          right: 0,
-                          left: 0,
-                          child: DotsIndicator(
-                            dotsCount: bannersWidget.length,
-                            position: _carouselIndex.toDouble(),
-                            decorator: DotsDecorator(
-                              size: const Size.square(9.0),
-                              activeSize: const Size(18.0, 9.0),
-                              color: Colors.white,
-                              activeShape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(5.0)),
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            print('constraint : ${constraints.maxWidth}');
+            return Container(
+              margin: constraints.maxWidth > 600 ? EdgeInsets.symmetric(horizontal: constraints.maxWidth * 0.25) : EdgeInsets.all(0),
+              child: Column(
+                children: [
+                  Container(
+                      color: Colors.white,
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.fromLTRB(10, 0, 10, 5),
+                      child: SectionOne()),
+                  Container(
+                      color: Colors.white,
+                      padding: EdgeInsets.only(left: 10, right: 10),
+                      margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                      child: Stack(
+                        children: [
+                          CarouselSlider(
+                              items: bannersWidget,
+                              options: CarouselOptions(
+                                aspectRatio: 16 / 9,
+                                viewportFraction: 1.0,
+                                initialPage: 0,
+                                enableInfiniteScroll: true,
+                                reverse: false,
+                                autoPlay: true,
+                                onPageChanged: (index, reason) {
+                                  setState(() {
+                                    _carouselIndex = index;
+                                  });
+                                },
+                                autoPlayInterval: Duration(seconds: 3),
+                                autoPlayAnimationDuration:
+                                Duration(milliseconds: 800),
+                                autoPlayCurve: Curves.fastOutSlowIn,
+                                enlargeCenterPage: true,
+                                scrollDirection: Axis.horizontal,
+                              )),
+                          Positioned(
+                            bottom: 0,
+                            top: 200,
+                            right: 0,
+                            left: 0,
+                            child: DotsIndicator(
+                              dotsCount: bannersWidget.length,
+                              position: _carouselIndex.toDouble(),
+                              decorator: DotsDecorator(
+                                size: const Size.square(9.0),
+                                activeSize: const Size(18.0, 9.0),
+                                color: Colors.white,
+                                activeShape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(5.0)),
+                              ),
                             ),
+                          )
+                        ],
+                      )),
+                  Container(
+                    color: Colors.white,
+                    padding: EdgeInsets.only(left: 30, right: 30),
+                    margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        IconMenu(
+                            image: 'assets/icons/icon-donasi.png', title: 'Donasi'),
+                        IconMenu(
+                            image: 'assets/icons/icon-zakat.png', title: 'Zakat'),
+                        IconMenu(
+                            image: 'assets/icons/menu_galang_dana.png',
+                            title: 'Galang Dana'),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    padding: EdgeInsets.only(left: 10, right: 10, bottom: 20, top: 10),
+                    margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text('Penggalangan Dana Mendesak ',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600)),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          height: 260,
+                          child: ListView(
+                            scrollDirection: Axis.horizontal,
+                            children: [
+                              DonationCard(title: 'Anak tukang becak ditabrak, KOMA & Kritis', image: 'assets/images/donasi 1.jpeg',
+                                  amount: 'Rp 34.231.080', progress: 0.2, remainDate: '8 hari lagi'),
+                              DonationCard(title: 'Anak tukang becak ditabrak, KOMA & Kritis', image: 'assets/images/donasi 2.jpeg',
+                                  amount: 'Rp 34.231.080', progress: 0.2, remainDate: '8 hari lagi'),
+                              DonationCard(title: 'Anak tukang becak ditabrak, KOMA & Kritis', image: 'assets/images/donasi 3.jpeg',
+                                  amount: 'Rp 34.231.080', progress: 0.2, remainDate: '8 hari lagi'),
+                            ],
                           ),
                         )
                       ],
-                    )),
-                Container(
-                  color: Colors.white,
-                  padding: EdgeInsets.only(left: 30, right: 30),
-                  margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconMenu(
-                          image: 'assets/icons/icon-donasi.png', title: 'Donasi'),
-                      IconMenu(
-                          image: 'assets/icons/icon-zakat.png', title: 'Zakat'),
-                      IconMenu(
-                          image: 'assets/icons/menu_galang_dana.png',
-                          title: 'Galang Dana'),
-                    ],
+                    ),
                   ),
-                ),
-                Container(
-                  color: Colors.white,
-                  padding: EdgeInsets.only(left: 10, right: 10, bottom: 20, top: 10),
-                  margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text('Penggalangan Dana Mendesak ',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600)),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        height: 260,
-                        child: ListView(
-                          scrollDirection: Axis.horizontal,
-                          children: [
-                            DonationCard(title: 'Anak tukang becak ditabrak, KOMA & Kritis', image: 'assets/images/donasi 1.jpeg',
-                                amount: 'Rp 34.231.080', progress: 0.2, remainDate: '8 hari lagi'),
-                            DonationCard(title: 'Anak tukang becak ditabrak, KOMA & Kritis', image: 'assets/images/donasi 2.jpeg',
-                                amount: 'Rp 34.231.080', progress: 0.2, remainDate: '8 hari lagi'),
-                            DonationCard(title: 'Anak tukang becak ditabrak, KOMA & Kritis', image: 'assets/images/donasi 3.jpeg',
-                                amount: 'Rp 34.231.080', progress: 0.2, remainDate: '8 hari lagi'),
-                          ],
-                        ),
-                      )
-                    ],
+                  Container(
+                    color: Colors.white,
+                    padding: EdgeInsets.only(left: 10, right: 10, bottom: 0, top: 0),
+                    margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                    child: Image.asset('assets/images/flashsale.png', fit: BoxFit.fitWidth,),
                   ),
-                ),
-                Container(
-                  color: Colors.white,
-                  padding: EdgeInsets.only(left: 10, right: 10, bottom: 0, top: 0),
-                  margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
-                  child: Image.asset('assets/images/flashsale.png', fit: BoxFit.fitWidth,),
-                ),
 
-                Container(
-                  color: Colors.white,
-                  padding: EdgeInsets.only(left: 10, right: 10, bottom: 20, top: 10),
-                  margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text('Program Spesial Kitabisa',
-                          style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 18,
-                              fontWeight: FontWeight.w600)),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        height: 260,
-                        child: ListView(
-                          scrollDirection: Axis.horizontal,
-                          children: [
-                            DonationCard(title: "Sedekah untuk penghapal Al-Qur'an", image: 'assets/images/program spesial1.jpeg',
-                                amount: 'Rp 34.231.080', progress: 0.2, remainDate: '8 hari lagi'),
-                            DonationCard(title: 'Selamatkan satwa liar korban perdagangan', image: 'assets/images/program spesial 2.jpeg',
-                                amount: 'Rp 34.231.080', progress: 0.2, remainDate: '8 hari lagi'),
-                            DonationCard(title: 'Anak tukang becak ditabrak, KOMA & Kritis', image: 'assets/images/program spesial 3.jpeg',
-                                amount: 'Rp 34.231.080', progress: 0.2, remainDate: '8 hari lagi'),
-                          ],
+                  Container(
+                    color: Colors.white,
+                    padding: EdgeInsets.only(left: 10, right: 10, bottom: 20, top: 10),
+                    margin: EdgeInsets.fromLTRB(10, 5, 10, 5),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text('Program Spesial Kitabisa',
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w600)),
+                        SizedBox(
+                          height: 10,
                         ),
-                      )
-                    ],
+                        Container(
+                          height: 260,
+                          child: ListView(
+                            scrollDirection: Axis.horizontal,
+                            children: [
+                              DonationCard(title: "Sedekah untuk penghapal Al-Qur'an", image: 'assets/images/program spesial1.jpeg',
+                                  amount: 'Rp 34.231.080', progress: 0.2, remainDate: '8 hari lagi'),
+                              DonationCard(title: 'Selamatkan satwa liar korban perdagangan', image: 'assets/images/program spesial 2.jpeg',
+                                  amount: 'Rp 34.231.080', progress: 0.2, remainDate: '8 hari lagi'),
+                              DonationCard(title: 'Anak tukang becak ditabrak, KOMA & Kritis', image: 'assets/images/program spesial 3.jpeg',
+                                  amount: 'Rp 34.231.080', progress: 0.2, remainDate: '8 hari lagi'),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
 
-              ],
-            ),
-          ),
+                ],
+              ),
+            );
+          }
         ),
       ),
       Text(
@@ -242,7 +242,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   const SizedBox(width: 20),
                   SizedBox(
                     height: 50,
-                    width: 400,
+                    width: kIsWeb ? 400 : 200,
                     child: TextField(
                       decoration: InputDecoration(
                         suffixIcon: IconButton(
